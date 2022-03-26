c06cc556aa0dbf572d79917c023b05ab21e500ad
Rumube <rumube@floridauniversitaria.es>
2022-03-26 09:57:34 +0100

    Revert "Merge branch 'main' into Ruben_AsteroidBlaster"
    This reverts commit 41d49518aaf9c194e44cb686a6c22d870faf1d11, reversing
    changes made to 1e8600f9e6b6c1ab2ac8fdbeee39fa2015bb42a1.

--- Server/Assets/Scripts/Utilities/Input/IInput.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IInput
-{
-    //We need the same methods for both inputs
-    public void Drag();
-}
