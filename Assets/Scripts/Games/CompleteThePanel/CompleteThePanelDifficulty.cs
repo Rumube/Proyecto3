b8c2411ab6b2e1fbbfc2defb5d57541d3ec308d2
Aimofe <78077349+Aimofe@users.noreply.github.com>
2022-03-28 12:41:13 +0200

    Difficulty

---
+++ Assets/Scripts/Games/CompleteThePanel/CompleteThePanelDifficulty.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompleteThePanelDifficulty : MonoBehaviour
+{
+    [Header("Data")]
+    public List<dataDiffilcuty> _dataDifficulty;
+
+    [Serializable]
+    public struct dataDiffilcuty
+    {
+        [Tooltip("Possible geometry that can be produced at this level")]
+        public List<Geometry.Geometry_Type> possibleGeometry;
+        [Tooltip("Possible geometry that can be target at this level")]
+        public List<Geometry.Geometry_Type> targetsGeometry;
+        [Tooltip("Total number of geometry at this level")]
+        public int numGeometry;
+        [Tooltip("Total number of targets at this level")]
+        public int numTargets;
+        [Tooltip("Total number of different geometry targets at this level")]
+        public int numGeometryTargets;
+
+    }
+
+    /// <summary>
+    /// Returns difficulty data to create the level
+    /// </summary>
+    /// <param name="levelDifficulty">Difficulty level</param>
+    /// <returns>Returns the data in format <see cref="dataDiffilcuty"/> </returns>
+    public dataDiffilcuty GenerateDataDifficulty(int levelDifficulty)
+    {
+        if (_dataDifficulty.Count < levelDifficulty)
+        {
+            return _dataDifficulty[0];
+        }
+        else
+        {
+            return _dataDifficulty[levelDifficulty - 1];
+        }
+    }
+
+}
