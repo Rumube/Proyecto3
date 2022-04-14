f585dd9ba40d39025ce3a5fd2dc637e531da3f83
Aimofe <aidamonfortef@gmail.com>
2022-04-14 18:27:58 +0200

    AddPanelDifficulty

---
+++ Assets/Scripts/Games/AddPanel/ButtonManager.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    [Header("Number of geometry target")]
+    public int _nButton;
+    
+
+    [Header("Number of geometry pressed")]
+    public int _buttonCounter;
+    
+    int _totalGeometry;
+    int _goodGeometry;
+    int _badGeometry;
+    public Text _mission;
+
+    public CreatePanelAddSubs _createPanel;
+    [SerializeField]
+    private CalculatePuntuation _calculatePuntuation;
+    [SerializeField]
+    private int _level;
+
+    [SerializeField]
+    AddPanelDifficulty _completeThePanel;
+    AddPanelDifficulty.dataDiffilcuty _currentDataDifficulty;
+
+    void Start()
+    {
+        //_completeThePanel = GetComponent<AddPanelDifficulty>();
+        _currentDataDifficulty = _completeThePanel.GenerateDataDifficulty(_level);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _mission.text = GeometryNumberText(_currentDataDifficulty.elementToAddSubs, "botón");
+    }
+
+    /// <summary>Show the geometry name in plural or singular.</summary> 
+
+    /// <param name="nGeometry">The quantity of a geometry</param> 
+    /// <param name="geometryName">The name of the geometry</param>
+
+    /// <returns>Empty or the name of the geometry in singular or plural</returns> 
+    public string GeometryNumberText(int nGeometry, string geometryName)
+    {
+        if (nGeometry == 0)
+        {
+            return "";
+        }
+        else if (nGeometry > 1)
+        {
+            return nGeometry + " botones";
+        }
+        else
+        {
+            return nGeometry + " botón";
+        }
+    }
+
+    /// <summary>Check the quantity of success.</summary> 
+    public void Compare()
+    {
+        ServiceLocator.Instance.GetService<GameManager>()._gameStateClient = GameManager.GAME_STATE_CLIENT.ranking;
+        CheckGeometry(_currentDataDifficulty.elementToAddSubs, _buttonCounter);
+       
+        _badGeometry = _totalGeometry - _goodGeometry;
+        _calculatePuntuation.Puntuation(_goodGeometry, _badGeometry);
+
+        _goodGeometry = 0;
+        _totalGeometry = 0;
+        _nButton = 0;
+
+        _createPanel.Restart();
+    }
+    /// <summary>Check how much geometry is ok.</summary> 
+    /// <param name="nGeometry">The quantity of a geometry</param> 
+    /// <param name="counter">The geometry of the player</param>
+    public void CheckGeometry(int nGeometry, int counter)
+    {
+        if (nGeometry > 0)
+        {
+            _totalGeometry += 1;
+            if (nGeometry == counter)
+            {
+                _goodGeometry += 1;
+            }
+        }
+
+    }
+    #region Button Counters
+    public void CounterButton(GameObject button)
+    {
+        _buttonCounter = Counter(button, _buttonCounter);
+    }
+   
+    public int Counter(GameObject button, int counter)
+    {
+        if (button.GetComponent<ObjectPanel>()._pressed == false)
+        {
+            counter++;
+            button.GetComponent<Image>().sprite = button.GetComponent<Button>().spriteState.pressedSprite;
+            button.GetComponent<ObjectPanel>()._pressed = true;
+        }
+        else
+        {
+            counter--;
+            button.GetComponent<Image>().sprite = button.GetComponent<Button>().spriteState.disabledSprite;
+            button.GetComponent<ObjectPanel>()._pressed = false;
+        }
+        return counter;
+    }
+    #endregion
+}
+
---
+++ Assets/Scripts/Games/AddPanel/CreatePanelAddSubs.cs
@@ -0,0 +1,200 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreatePanelAddSubs : MonoBehaviour
+{
+    [Header("Table Configuration")]
+    public int _column;
+    public int _row;
+    public int _gapX;
+    public int _gapY;
+    public int _offsetX;
+    public int _offsetY;
+
+    int _count;
+
+    public GameObject _canvas;
+    [Header("Geometry")]
+    public GameObject button;
+    public GameObject[] _geometryForms;
+    public List<GameObject> _geometryList = new List<GameObject>();
+    public List<GameObject> _targetList = new List<GameObject>();
+    public List<GameObject> _allList = new List<GameObject>();
+    private List<Geometry.Geometry_Type> _typeTargetGeometry = new List<Geometry.Geometry_Type>();
+
+    //Game Configuration
+    [SerializeField]
+    private int _level;
+
+    [SerializeField]
+    AddPanelDifficulty _completeThePanel;
+    AddPanelDifficulty.dataDiffilcuty _currentDataDifficulty;
+
+    public ButtonManager buttonCounter;
+    void Start()
+    {
+        _completeThePanel = GetComponent<AddPanelDifficulty>();
+        _currentDataDifficulty = _completeThePanel.GenerateDataDifficulty(_level);
+        GeneratePanel();
+    }
+
+    /// <summary>
+    /// Creates a panel with geometry.
+    /// </summary>
+    void GeneratePanel()
+    {
+        _geometryList.Clear();
+        _targetList.Clear();
+        _allList.Clear();
+        GenerateTargets();
+        GenerateNoTargetGeometry();
+
+        //COLOCAR BOTONES EN POSICIÓN
+
+        for (int x = 0; x < _column; x++)
+        {
+            for (int y = 0; y < _row; y++)
+            {
+                _allList[_count].GetComponent<Transform>().position = new Vector3((x + _offsetX) * _gapX, (y + _offsetY) * _gapY, 0);
+                _count++;
+               
+            }
+        }
+        ServiceLocator.Instance.GetService<IGameTimeConfiguration>().StartGameTime();
+    }
+
+    /// <summary>
+    /// Generates the normal geometry.
+    /// </summary>
+    private void GenerateNoTargetGeometry()
+    {
+        for (int i = 0; i < (_row * _column) - _currentDataDifficulty.pressedButtons; i++)
+        {
+            GameObject newGeometry;
+            newGeometry = Instantiate(button, new Vector3(0, 0, 0), Quaternion.identity);
+            newGeometry.transform.SetParent(_canvas.transform, false);
+            _geometryList.Add(newGeometry);
+            _allList.Add(newGeometry);
+        }
+
+    }
+    /// <summary>
+    /// Generates the target geometry.
+    /// </summary>
+    private void GenerateTargets()
+    {
+        //GenerateTypeTargetGeometry();
+        //do
+        //{
+            for (int i = 0; i < _allList.Count; i++)
+            {
+                Destroy(_allList[i]);
+            }
+            _targetList.Clear();
+            _allList.Clear();
+            for (int i = 0; i < _currentDataDifficulty.pressedButtons; i++)
+            {
+                int idGeometry = Random.Range(0, _currentDataDifficulty.targetGeometry.Count);
+                GameObject newGeometry = Instantiate(button, new Vector3(0, 0, 0), Quaternion.identity);
+                buttonCounter._buttonCounter = 1 + buttonCounter._buttonCounter;
+                newGeometry.GetComponent<Image>().sprite = newGeometry.GetComponent<Button>().spriteState.pressedSprite;
+                newGeometry.GetComponent<ObjectPanel>()._placed = false;
+                newGeometry.GetComponent<ObjectPanel>()._pressed = true;
+                _targetList.Add(newGeometry);
+                _allList.Add(newGeometry);
+                newGeometry.transform.SetParent(_canvas.transform, false);
+            }
+        //} while (!CheckTargets());
+    }
+    /// <summary>
+    /// Generates the geometry type of the target.
+    /// </summary>
+    //private void GenerateTypeTargetGeometry()
+    //{
+    //    _typeTargetGeometry.Clear();
+    //    for (int i = 0; i < 1; i++)
+    //    {
+    //        bool isCorrect = false;
+    //        int idGeometry = Random.Range(0, _currentDataDifficulty.targetGeometry.Count);
+    //        do
+    //        {
+    //            isCorrect = false;
+    //            Geometry.Geometry_Type newGeometry = _currentDataDifficulty.targetGeometry[idGeometry].GetComponent<Geometry>()._geometryType;
+    //            if (!_typeTargetGeometry.Contains(newGeometry))
+    //            {
+    //                _typeTargetGeometry.Add(newGeometry);
+    //                isCorrect = true;
+    //            }
+    //            else
+    //            {
+    //                idGeometry++;
+    //                if (idGeometry >= _currentDataDifficulty.targetGeometry.Count)
+    //                {
+    //                    idGeometry = 0;
+    //                }
+    //            }
+    //        } while (!isCorrect);
+    //    }
+
+    //}
+    ///// <summary>
+    ///// Checks if the list of targets contains geometry repeated.
+    ///// </summary>
+    //private bool CheckTargets()
+    //{
+    //    bool result = true;
+    //    List<Geometry.Geometry_Type> geoTypeTarget = new List<Geometry.Geometry_Type>();
+    //    foreach (GameObject currentTarget in _targetList)
+    //    {
+    //        geoTypeTarget.Add(currentTarget.GetComponent<Geometry>()._geometryType);
+    //    }
+    //    foreach (Geometry.Geometry_Type currentGeometryTpe in _typeTargetGeometry)
+    //    {
+    //        if (!geoTypeTarget.Contains(currentGeometryTpe))
+    //        {
+    //            result = false;
+    //        }
+    //    }
+    //    return result;
+    //}
+    // Update is called once per frame
+    void Update()
+    {
+        if (_allList.Count == _row * _column)
+        {
+            if (ServiceLocator.Instance.GetService<GameManager>()._gameStateClient != GameManager.GAME_STATE_CLIENT.playing)
+            {
+                for (int i = 0; i < _allList.Count; i++)
+                {
+                    _allList[i].GetComponent<Button>().interactable = false;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < _allList.Count; i++)
+                {
+                    _allList[i].GetComponent<Button>().interactable = true;
+                }
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Restarts the minigame.
+    /// </summary>
+    public void Restart()
+    {
+        foreach (GameObject geometry in _allList)
+        {
+            Destroy(geometry);
+        }
+        _count = 0;
+        GeneratePanel();
+    }
+}
+
+
+
