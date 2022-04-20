a2024a6f3a3b41856f56e7a8176366347c4dd826
Rumube <rumube@floridauniversitaria.es>
2022-04-20 17:07:09 +0200

    Adding a pile to the messages

--- Assets/Scripts/UI/FrogMessage.cs
+++ Assets/Scripts/UI/FrogMessage.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Crosstales.RTVoice;
+using Crosstales.RTVoice.Model;
 
 public class FrogMessage : MonoBehaviour, IFrogMessage
 {
     AudioSource _audio;
     Text _textMessage;
     Image _imageMassage;
+    [SerializeField]
+    public List<string> _messagePile = new List<string>();
+    private bool _messageAtive;
 
     public float _timeWriting;
     public float _messageDuration;
@@ -20,24 +24,51 @@ public class FrogMessage : MonoBehaviour, IFrogMessage
         _audio = GetComponent<AudioSource>();
         Speaker.Instance.VoiceForCulture("es-Es");
         _audio.pitch = 1.2f;
+        _messageAtive = false;
+        Speaker.Instance.OnSpeakComplete += MessageComplete;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_messagePile.Count >= 1 && !_messageAtive)
+        {
+            //if(_messagePile.Count > 1)
+            //{
+            //    int count = 0;
+            //    List<string> deleteList = new List<string>(_messagePile);
+            //    while (_messagePile[0] == _messagePile[count])
+            //    {
+            //        deleteList.Remove(_messagePile[count]);
+            //        count++;
+            //    }
+            //    _messagePile = new List<string>(deleteList);
+            //}
+
+            _messageAtive = true;
+            SendMessage(_messagePile[0]);
+            _messagePile.Remove(_messagePile[0]);
+        }
     }
 
+
+    private void SendMessage(string message)
+    {
+        _textMessage = GameObject.FindGameObjectWithTag("OrderText").GetComponent<Text>();
+        _imageMassage = GameObject.FindGameObjectWithTag("OrderImage").GetComponent<Image>();
+        StartCoroutine(FrogCoroutine(message));
+    }
+    private void MessageComplete(Wrapper wrapper)
+    {
+        StartCoroutine(CloseMessage());
+    }
     /// <summary>
     /// Start the messages process
     /// </summary>
     /// <param name="message">The message text</param>
     public void NewFrogMessage(string message)
     {
-        StopAllCoroutines();
-        _textMessage = GameObject.FindGameObjectWithTag("OrderText").GetComponent<Text>();
-        _imageMassage = GameObject.FindGameObjectWithTag("OrderImage").GetComponent<Image>();
-        StartCoroutine(FrogCoroutine(message));
+        _messagePile.Add(message);
     }
     /// <summary>
     /// Start the messages process and save the message in <see cref="_lastOrder"/>
@@ -46,12 +77,9 @@ public class FrogMessage : MonoBehaviour, IFrogMessage
     /// <param name="isOrder">The message is an order</param>
     public void NewFrogMessage(string message, bool isOrder)
     {
-        StopAllCoroutines();
-        _textMessage = GameObject.FindGameObjectWithTag("OrderText").GetComponent<Text>();
-        _imageMassage = GameObject.FindGameObjectWithTag("OrderImage").GetComponent<Image>();
-        StartCoroutine(FrogCoroutine(message));
         if (isOrder)
             _lastOrder = message;
+        _messagePile.Add(message);
     }
 
     /// <summary>
@@ -75,7 +103,6 @@ public class FrogMessage : MonoBehaviour, IFrogMessage
 
             yield return new WaitForSeconds(waitTime);
         }
-        StartCoroutine(CloseMessage());
     }
     /// <summary>
     /// Repeat the last order gived by Min
@@ -93,5 +120,6 @@ public class FrogMessage : MonoBehaviour, IFrogMessage
         yield return new WaitForSeconds(_messageDuration);
         _textMessage.text = "";
         _imageMassage.color = new Color(0, 0, 0, 0);
+        _messageAtive = false;
     }
 }
