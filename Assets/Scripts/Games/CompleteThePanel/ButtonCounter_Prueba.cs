6587b0344136283c1c39dcd2faf0c2b6ecb5a044
ccpdrawer <carmencasan2001@gmail.com>
2022-04-26 13:24:20 +0200

    Finished game

--- Assets/Scripts/Games/CompleteThePanel/ButtonCounter_Prueba.cs
+++ Assets/Scripts/Games/CompleteThePanel/ButtonCounter_Prueba.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ButtonCounter : MonoBehaviour
+public class ButtonCounter_Prueba : MonoBehaviour
 {
     [Header("Number of geometry target")]
     public int _nSquare;
@@ -28,7 +28,7 @@ public class ButtonCounter : MonoBehaviour
     int _badGeometry;
     public Text _mission;
 
-    public CreatePanel _createPanel;
+    public CreatePanel_Prueba _createPanel;
     [SerializeField]
     private CalculatePuntuation _calculatePuntuation;
 
@@ -52,21 +52,17 @@ public class ButtonCounter : MonoBehaviour
     /// <param name="geometryName">The name of the geometry</param>
 
     /// <returns>Empty or the name of the geometry in singular or plural</returns> 
-    public string GeometryNumberText(int nGeometry,string geometryName)
+    public string GeometryNumberText(int nGeometry, string geometryName)
     {
-        Debug.Log("He entrado en GeometryNumber");
-        if (nGeometry==0)
+        if (nGeometry == 0)
         {
             return "";
         }
-        else if (nGeometry>1)
-        {
-            return nGeometry+" "+geometryName + "s ";
-        }
         else
-        {
-            return nGeometry+" "+ geometryName+" ";
+        { 
+            return geometryName + "s ";
         }
+    
     }
 
     /// <summary>Check the quantity of success.</summary> 
@@ -85,7 +81,7 @@ public class ButtonCounter : MonoBehaviour
         ServiceLocator.Instance.GetService<IPositive>().GenerateFeedback(Vector2.zero);
         _goodGeometry = 0;
         _totalGeometry = 0;
-        _nSquare=0;
+        _nSquare = 0;
         _nTriangle = 0;
         _nCircle = 0;
         _nDiamond = 0;
@@ -115,50 +111,50 @@ public class ButtonCounter : MonoBehaviour
                 _goodGeometry += 1;
             }
         }
-       
+
     }
     #region Button Counters
     public void CounterSquare(GameObject button)
     {
-        _squareCounter=Counter(button, _squareCounter);
+        _squareCounter = Counter(button, _squareCounter);
     }
     public void CounterTriangle(GameObject button)
     {
-        _triangleCounter=Counter(button, _triangleCounter);
+        _triangleCounter = Counter(button, _triangleCounter);
     }
     public void CounterCircle(GameObject button)
     {
-        _circleCounter= Counter(button, _circleCounter);
+        _circleCounter = Counter(button, _circleCounter);
     }
     public void CounterDiamond(GameObject button)
     {
-        _diamondCounter=Counter(button, _diamondCounter);
+        _diamondCounter = Counter(button, _diamondCounter);
     }
     public void CounterRectangle(GameObject button)
     {
-        _rectangleCounter=Counter(button, _rectangleCounter);
+        _rectangleCounter = Counter(button, _rectangleCounter);
     }
     public void CounterPentagon(GameObject button)
     {
-        _pentagonCounter= Counter(button, _pentagonCounter);
+        _pentagonCounter = Counter(button, _pentagonCounter);
     }
     public void CounterHexagon(GameObject button)
     {
-       _hexagonCounter= Counter(button, _hexagonCounter);
+        _hexagonCounter = Counter(button, _hexagonCounter);
     }
     public int Counter(GameObject button, int counter)
     {
-        if (button.GetComponent<ObjectPanel>()._pressed == false)
+        if (button.GetComponent<ObjectPanel_Prueba>()._pressed == false)
         {
             counter++;
             button.GetComponent<Image>().sprite = button.GetComponent<Button>().spriteState.pressedSprite;
-            button.GetComponent<ObjectPanel>()._pressed = true;
+            button.GetComponent<ObjectPanel_Prueba>()._pressed = true;
         }
         else
         {
             counter--;
             button.GetComponent<Image>().sprite = button.GetComponent<Button>().spriteState.disabledSprite;
-            button.GetComponent<ObjectPanel>()._pressed = false;
+            button.GetComponent<ObjectPanel_Prueba>()._pressed = false;
         }
         return counter;
     }
