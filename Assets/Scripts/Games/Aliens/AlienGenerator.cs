e19b15e519041b5ad8284f60d12d744aa453860e
carlacoronado207 <cacobe@floridauniversitaria.es>
2022-05-02 12:52:37 +0200

    Creating telescope asociation game
    Scene assebled
    Started programming buttons

---
+++ Assets/Scripts/Games/Aliens/AlienGenerator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienGenerator : MonoBehaviour
+{
+    [Header("Configuration")]
+    public GameObject _eye;
+    public GameObject _arm;
+    public GameObject _leg;
+    public GameObject _mouth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
