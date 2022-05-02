c0e353cfd4fd55fd742bc13f6c4bde21df177b33
carlacoronado207 <cacobe@floridauniversitaria.es>
2022-05-02 13:39:16 +0200

    Updating Telescope asociation

--- Assets/Scripts/Games/Aliens/AlienGenerator.cs
+++ Assets/Scripts/Games/Aliens/AlienGenerator.cs
@@ -4,20 +4,52 @@ using UnityEngine;
 
 public class AlienGenerator : MonoBehaviour
 {
-    [Header("Configuration")]
-    public GameObject _eye;
-    public GameObject _arm;
-    public GameObject _leg;
-    public GameObject _mouth;
+    [Header("ForTextComprobation")]
+    public GameObject _eyeText;
+    public GameObject _armText;
+    public GameObject _legText;
+    public GameObject _mouthText;
+
+    [Header("AlienParts")]
+    public GameObject _alienBase;
+    public GameObject _alienEye;
+    public GameObject _alienArm;
+    public GameObject _alienLeg;
+    public GameObject _alienMouth;
+
+    private RectTransform alienposition;
 
     // Start is called before the first frame update
     void Start()
     {
+        alienposition = _alienBase.GetComponent<RectTransform>();
+        GenerateAlien(2, 3, 5, 4);
     }
 
     private void Update()
     {
         
+    }
+
+    private void GenerateAlien(int arm, int leg, int mouth , int eye)
+    {
+        for (int i = 0; i < arm; i++)
+        {
+            Instantiate(_alienArm, alienposition);
+        }
+        for (int i = 0; i < leg; i++)
+        {
+            Instantiate(_alienLeg, alienposition);
+        }
+        for (int i = 0; i < mouth; i++)
+        {
+            Instantiate(_alienMouth, alienposition);
+        }
+        for (int i = 0; i < eye; i++)
+        {
+            Instantiate(_alienEye, alienposition);
+        }
+
     }
 
 }
