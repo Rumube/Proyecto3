d52512697f0bca38d83d35db3b509e95f77829a1
AnthonY0690 <anhero@floridauniversitaria.es>
2022-05-09 13:33:04 +0200

    music test
    music test

--- Assets/Life.cs
+++ Assets/Life.cs
@@ -2,24 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundEffect : MonoBehaviour
+public class Life : MonoBehaviour
 {
-
-    public GameObject starSound;
+    public float lifeTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, lifeTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0)) 
-        {
-            Instantiate(starSound);
-            
-        }
+        
     }
 }
