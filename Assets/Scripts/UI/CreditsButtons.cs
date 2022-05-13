eedb0e2a37f00943a7a55e0dbe7c2a7813a925db
ccpdrawer <80060932+ccpdrawer@users.noreply.github.com>
2022-05-13 13:14:01 +0200

    Update buttons

--- Assets/Scripts/UI/CreditsButtons.cs
+++ Assets/Scripts/UI/CreditsButtons.cs
@@ -7,23 +7,15 @@ using TMPro;
 public class CreditsButtons : MonoBehaviour
 {
     public GameObject _creditPanel;
-    //public Animator _panelAnim;
     public string _eachText;
     public TMP_Text _panelText;
     int _showingValue;
 
-    // Start is called before the first frame update
     void Start()
     {
         _creditPanel.SetActive(false);
         _showingValue = 0;
        _panelText.text = _eachText;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void ShowMessage()
@@ -32,13 +24,13 @@ public class CreditsButtons : MonoBehaviour
         {
             _creditPanel.SetActive(true);
             _showingValue = 1;
-            //_panelAnim.Play("Appear");
+     
         }
         else 
         {
             _creditPanel.SetActive(false);
             _showingValue = 0;
-            //_panelAnim.Play("Disappear");
+           
         }
     }
 
