ade54ccd0c415572ee600ee6c2cec48ed54e336e
Carla <carlacoronado77@gmail.com>
2022-05-30 13:49:17 +0200

    Tutorial & mobile UI changes

---
+++ Assets/Scripts/UI/Tutorial.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject _eachScreen;
+    int _showingValue;
+
+    public GameObject ClickSound;
+
+
+
+    void Start()
+    {
+        _eachScreen.SetActive(false);
+        _showingValue = 0;
+       
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(ClickSound);
+        }
+    }
+
+    public void ShowScreen()
+    {
+        if (_showingValue == 0)
+        {
+            _eachScreen.SetActive(true);
+            _showingValue = 1;
+        }
+        else
+        {
+            _eachScreen.SetActive(false);
+            _showingValue = 0;
+        }
+    }
+
+    public void BackScreen()
+    {
+        _eachScreen.SetActive(false);
+        _showingValue = 0;
+    }
+}
