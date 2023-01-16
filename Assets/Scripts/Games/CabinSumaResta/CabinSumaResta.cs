948292c233af03bf2c9c8198b890fe103ce4b6f5
rubén <rumube@floridauniversitaria.es>
2023-01-16 08:36:06 +0100

    Created scripts

---
+++ Assets/Scripts/Games/CabinSumaResta/CabinSumaResta.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CabinSumaResta : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GenerateTarget()
+    {
+
+    }
+
+    private void GenerateAsteroids()
+    {
+
+    }
+
+    private void CheckIfIsCorrect()
+    {
+
+
+    }
+
+}
