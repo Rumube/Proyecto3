b49a7c75724f5559d1649a626370214ce2687f4f
rubén <rumube@floridauniversitaria.es>
2023-01-16 13:26:28 +0100

    Programming animations

--- Assets/Scripts/Games/CabinSumaResta/PickableAsteroid.cs
+++ Assets/Scripts/Games/CabinSumaResta/PickableAsteroid.cs
@@ -7,27 +7,23 @@ public class PickableAsteroid : MonoBehaviour
     [Header("References")]
     public List<Sprite> _spriteLsit = new List<Sprite>();
     public GameObject _selectIcon;
+    public Animator _anim;
     [Header("State")]
     private bool _selected = false;
     // Start is called before the first frame update
     void Start()
     {
+        _anim.GetComponent<Animator>();
         SetSprite();
         transform.Rotate(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
         transform.localScale = Vector3.one * Random.Range(0.7f, 1f);
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         _selectIcon.SetActive(_selected);
+        if (_selected)
+        {
+            _anim.Play("RedFrameSelect_animation");
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        print("Seleccionado");
-    }
 
     private void SetSprite()
     {
@@ -37,6 +33,21 @@ public class PickableAsteroid : MonoBehaviour
     public void SelectAsteroid()
     {
         _selected = !_selected;
+
+        if (_selected)
+        {
+            _selectIcon.SetActive(true);
+            _anim.Play("RedFrameSelect_animation");
+        }
+        else{
+            _anim.Play("RedFrameDeselect_animation");
+            StartCoroutine(DeselectAnimController());
+        }
+    }
+    private IEnumerator DeselectAnimController()
+    {
+        yield return new WaitForSeconds(1f);
+        _selectIcon.SetActive(false);
     }
     public bool GetSelected()
     {
